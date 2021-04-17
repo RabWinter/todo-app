127b6011d21ed3cdc496f49ec04f5bdd15b66ae5
Rab Winter <77572162+RabWinter@users.noreply.github.com>
2021-04-17 16:47:24 +0100

    Update script.js

--- app/js/script.js
+++ app/js/script.js
@@ -9,7 +9,7 @@
  const todoCount = document.getElementById('count');
  const clear = document.getElementById('clear__completed');
  const theme = document.getElementById('theme');
-let radioChecked = document.querySelector(
+const radioChecked = document.querySelector(
  '.form__item input[type="radio"]:checked'
 );
 
@@ -29,12 +29,12 @@ let radioChecked = document.querySelector(
  </label>
  <span class="form__item--remove" id="remove"></span>`;
 
-//    if (
-//     radioChecked.id ===
-//     'completed'
-//    ) {
-//     newTodo.classList.add('hidden');
-//    }
+   if (
+    radioChecked.id ===
+    'completed'
+   ) {
+    newTodo.classList.add('hidden');
+   }
    formList.appendChild(newTodo);
    updateCount(1);
 
